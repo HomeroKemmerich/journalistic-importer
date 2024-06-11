5bac1ddde9a9d20b5448dea1f8373eeccc4ca7ef
HomeroKemmerich <hmvkemmerich@gmail.com>
2024-06-11 16:38:53 -0300

    Settings model

---
+++ src/JournalisticImporterSettings.ts
@@ -0,0 +1,8 @@
+export interface JournalisticImporterSettings {
+	entriesDestinationFolder: string;
+	peopleDestinationFolder: string;
+	notesDestinationFolder: string;
+	goalsDestinationFolder: string;
+	gemsDestinationFolder: string;
+	ideasDestinationFolder: string;
+}
